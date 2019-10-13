04947281b4b85de5c319a9f18b5ff4041404dd90
ianbeer <beer_ian@hotmail.com>
2019-10-13 17:06:46 +0100

    added test for doubling function

---
+++ src/main/java/org/ian/coding/bat/Functional1.java
@@ -0,0 +1,11 @@
+package org.ian.coding.bat;
+
+import java.util.List;
+
+public class Functional1 {
+    //Given a list of integers, return a list where each integer is multiplied by 2.
+    public List<Integer> doubling(List<Integer> nums) {
+
+        return null;
+    }
+}
---
+++ src/test/java/org/ian/coding/bat/Functional1Test.java
@@ -0,0 +1,36 @@
+package org.ian.coding.bat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class Functional1Test {
+    private Functional1 underTest;
+
+    @Before
+    public void setUp() throws Exception {
+        underTest = new Functional1();
+    }
+
+    @Test
+    public void shouldDoubling() throws Exception {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+
+        List<Integer> ret = underTest.doubling(nums);
+
+        assertThat(ret.get(0), is(2));
+        assertThat(ret.get(1), is(4));
+        assertThat(ret.get(2), is(6));
+
+
+    }
+
+}
