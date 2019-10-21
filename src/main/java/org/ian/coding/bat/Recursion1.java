3fe60669710dac9cee58a434c66d2a837e55c76c
ianbeer <beer_ian@hotmail.com>
2019-10-21 12:14:02 +0100

    added bunnyEars body

--- src/main/java/org/ian/coding/bat/Recursion1.java
+++ src/main/java/org/ian/coding/bat/Recursion1.java
@@ -11,6 +11,10 @@ public class Recursion1 {
     }
 
     public int bunnyEars(int bunnies) {
+        if (bunnies == 0) {
+            return 0;
+        }
 
+        return 2+bunnyEars(bunnies-1);
     }
 }
