bc9d20b9a1f32f612f643d4e246f2824b78730e8
ianbeer <beer_ian@hotmail.com>
2019-10-21 07:06:56 +0100

    added no neg

--- src/main/java/org/ian/coding/bat/Functional2.java
+++ src/main/java/org/ian/coding/bat/Functional2.java
@@ -1,10 +1,11 @@
 package org.ian.coding.bat;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Functional2 {
 
     public List<Integer> noNeg(List<Integer> nums) {
-
+        return nums.stream().filter(i -> i >=0).collect(Collectors.toList());
     }
 }
