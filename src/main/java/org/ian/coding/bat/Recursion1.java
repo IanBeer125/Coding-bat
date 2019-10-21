e803086185160752f8f66fca37ab8dc6899fff4c
ianbeer <beer_ian@hotmail.com>
2019-10-21 13:57:33 +0100

    added array6

--- src/main/java/org/ian/coding/bat/Recursion1.java
+++ src/main/java/org/ian/coding/bat/Recursion1.java
@@ -151,7 +151,15 @@ public class Recursion1 {
     }
 
     public boolean array6(int[] nums, int index) {
+        if (index >= nums.length) {
+            return false;
+        }
 
+        if (nums[index] == 6) {
+            return true;
+        } else  {
+            return array6(nums, index+1);
+        }
     }
 
 
